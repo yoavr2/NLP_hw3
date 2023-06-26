62d791c6afc526f7ded3fee4aeba43e4d3df7c3d
yoav <yoavr1106@gmail.com>
2023-06-26 16:25:25 +0300

    question 2 part 4 in code and parts 4,6 in word

--- pretrained_transformers/src/london_baseline.py
+++ pretrained_transformers/src/london_baseline.py
@@ -2,3 +2,12 @@
 #   example in the dev set.
 # Hint: Make use of existing code.
 # Your solution here should only be a few lines.
+import utils
+from tqdm import tqdm
+
+path = "../birth_dev.tsv"
+predictions = ["London" for line in tqdm(open(path, encoding='utf-8'))]
+
+total, correct = utils.evaluate_places(path, predictions)
+
+print('Correct: {} out of {}: {}%'.format(correct, total, correct / total * 100))
